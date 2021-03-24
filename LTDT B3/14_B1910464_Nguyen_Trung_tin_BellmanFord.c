7acf2484994de16a03844c5744b2e4b016e1601c
justt1n <t1nnguy3n0709@gmail.com>
2021-03-24 21:05:40 +0700

    xong bai 2 buoi 3

---
+++ LTDT B3/14_B1910464_Nguyen_Trung_tin_BellmanFord.c
@@ -0,0 +1,114 @@
+
+#include <stdio.h>
+#define MAX_M 500
+typedef struct
+{
+    int u, v, w;
+} Edge;
+
+typedef struct
+{
+    Edge edges[MAX_M];
+    int n, m;
+} Graph;
+
+void init_graph(Graph *pG, int n)
+{
+    pG->n = n;
+    pG->m = 1;
+}
+
+void add_edge(Graph *pG, int x, int y, int w)
+{
+    pG->edges[pG->m].u = x;
+    pG->edges[pG->m].v = y;
+    pG->edges[pG->m].w = w;
+    pG->m++;
+}
+
+int degree(Graph *pG, int x)
+{
+    int count = 0;
+    for (int i = 0; i <= pG->m; i++)
+    {
+        if (pG->edges[i].u == x && pG->edges[i].v != x)
+            count++;
+        if (pG->edges[i].u != x && pG->edges[i].v == x)
+            count++;
+    }
+    return count;
+}
+
+int adjacent(Graph *pG, int x, int y)
+{
+    for (int i = 0; i < pG->m; i++)
+    {
+        if (pG->edges[i].u == x && pG->edges[i].v == y)
+            return 1;
+        if (pG->edges[i].u == y && pG->edges[i].v == x)
+            return 1;
+    }
+    return 0;
+}
+
+void neighbors(Graph *pG, int x)
+{
+    for (int i = 1; i <= pG->n; i++)
+        if (adjacent(pG, i, x) == 1 && x != i)
+            printf("%d ", i);
+}
+
+#define INFINITY 9999999
+#define MAXN 100
+
+int pi[MAXN];
+int p[MAXN];
+// Co chu trinh am hay khong
+void BellmanFord(Graph *pG, int s)
+{
+    int u, v, w;
+    for (int i = 1; i <= pG->n; i++)
+        pi[i] = INFINITY;
+    pi[s] = 0;
+    p[s] = -1;
+    for (int i = 1; i <= pG->n; i++)
+    {
+        for (int k = 1; k <= pG->m; k++)
+        {
+            u = pG->edges[k].u;
+            v = pG->edges[k].v;
+            w = pG->edges[k].w;
+            if (pi[u] + w < pi[v])
+            {
+                pi[v] = pi[u] + w;
+                p[v] = u;
+            }
+        }
+    }
+    // for (int k = 1; k <= pG->m; k++)
+    // {
+    //     u = pG->edges[k].u;
+    //     v = pG->edges[k].v;
+    //     w = pG->edges[k].w;
+    //     if (pi[u] + w < pi[v])
+    //         return 1;
+    // }
+    // return 0;
+}
+
+int main()
+{
+    Graph G;
+    int n, m, u, v, w, e;
+    scanf("%d%d", &n, &m);
+    init_graph(&G, n);
+
+    for (e = 0; e < m; e++)
+    {
+        scanf("%d%d%d", &u, &v, &w);
+        add_edge(&G, u, v, w);
+    }
+    BellmanFord(&G, 1);
+    printf("%d\n", pi[n]);
+    return 0;
+} 
---
+++ LTDT B3/14_B1910464_Nguyen_Trung_tin_MooreDijkstra.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+
+#define MAX_ELEMENTS 100
+typedef int ElementType;
+typedef struct
+{
+    ElementType data[MAX_ELEMENTS];
+    int size;
+} List;
+/* Tao danh sach rong */
+void make_null(List *L)
+{
+    L->size = 0;
+}
+/* Them mot phan tu vao cuoi danh sach */
+void push_back(List *L, ElementType x)
+{
+    L->data[L->size] = x;
+    L->size++;
+}
+/* Lay phan tu tai vi tri i, phan tu bat dau o vi tri 1 */
+ElementType element_at(List *L, int i)
+{
+    return L->data[i - 1];
+}
+/* Tra ve so phan tu cua danh sach */
+int count_list(List *L)
+{
+    return L->size;
+}
+
+int member(List *L, int x)
+{
+    for (int i = 0; i < L->size; i++)
+        if (L->data[i] == x)
+            return 1;
+    return 0;
+}
+
+#define MAX_VERTEXES 100
+typedef struct
+{
+    int A[100][100];
+    int n;
+} Graph;
+
+void init_graph(Graph *pG, int n)
+{
+    pG->n = n;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            pG->A[i][j] = 0;
+}
+
+void add_edge(Graph *pG, int x, int y, int w)
+{
+    pG->A[x][y] = w;
+}
+
+int degree(Graph *pG, int x)
+{
+    int deg = 0;
+    for (int i = 1; i <= pG->n; i++)
+        if (pG->A[x][i] > 0)
+            deg += 1;
+    return deg;
+}
+
+int adjacent(Graph *pG, int x, int y)
+{
+    return pG->A[x][y];
+}
+
+List neighbors(Graph *pG, int x)
+{
+    List L;
+    make_null(&L);
+    for (int i = 1; i <= pG->n; i++)
+        if (adjacent(pG, x, i) == 1 && x != i)
+            push_back(&L, i);
+    return L;
+}
+
+void printM(Graph *pG)
+{
+    for (int i = 1; i <= pG->n; i++)
+    {
+        for (int j = 1; j <= pG->n; j++)
+            printf("%d ", pG->A[i][j]);
+        printf("\n");
+    }
+}
+
+int mark[100]; //mang danh dau
+int pi[100];   //chieu dai tu dinh xuat phat den u
+int p[100];    //dinh truoc u
+
+void dijkstra(Graph *pG, int s)
+{
+    int i, j, k;
+    for (i = 1; i <= pG->n; i++)
+    {
+        pi[i] = 99999;
+        mark[i] = 0;
+    }
+    pi[s] = 0;
+    p[s] = -1;
+
+    for (k = 1; k <= pG->n; k++)
+    {
+        int min_pi = 99999;
+        for (j = 1; j <= pG->n; j++)
+        {
+            if (mark[j] == 0 && pi[j] < min_pi)
+            {
+                min_pi = pi[j];
+                i = j;
+            }
+        }
+        mark[i] = 1;
+        for (int j = 1; j <= pG->n; j++)
+            if (pG->A[i][j] != 0 && mark[j] == 0)
+                if (pi[i] + pG->A[i][j] < pi[j])
+                {
+                    pi[j] = pi[i] + pG->A[i][j];
+                    p[j] = i;
+                }
+    }
+}
+
+int main()
+{
+    Graph G;
+    int n, m, u, v, w, e;
+    scanf("%d%d", &n, &m);
+    init_graph(&G, n);
+    for (e = 1; e <= m; e++)
+    {
+        scanf("%d%d%d", &u, &v, &w);
+        add_edge(&G, u, v, w);
+    }
+    dijkstra(&G, 1);
+    printf("%d", mark[n] ? pi[n] : -1);
+    return 0;
+}
